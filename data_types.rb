72525368c08a75c6729d951e95b6fc482bf0bebb
Ella Grimshaw <ella@stripe.com>
2016-09-27 19:23:04 -0700

    Ruby data examples

---
+++ data_types.rb
@@ -0,0 +1,16 @@
+name = "Ella"
+
+age = 23
+
+puts "Do you know Ruby?"
+knows_ruby = gets.chomp
+if knows_ruby == "y"
+	knows_ruby = true
+end
+
+siblings = ["Miles", "Nathan", "Eve", "Anya"]
+
+card = {
+	exp_year: 2020,
+	exp_month: 07
+}
